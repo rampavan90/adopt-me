4548bbfe682eb04d266473cb26ce7abfd9005645
Ram Pavan FLIP <rampavan.kumar@learnwithflip.com>
2019-09-17 17:27:44 +0530

    modal error- fix it

--- src/Pet.js
+++ src/Pet.js
@@ -1,9 +1,11 @@
 import React from "react";
-import {Link } from "@reach/router";
-export default function Pet({ name, animal, breed, media, location, id }) {
+import { Link } from "@reach/router";
 
-  let hero = 'http://placecorgi.com/300/300';
-  if(media.length) {
+const Pet = props => {
+  const { name, animal, breed, media, location, id } = props;
+
+  let hero = "http://placecorgi.com/300/300";
+  if (media.length) {
     hero = media[0].small;
   }
 
@@ -14,9 +16,10 @@ export default function Pet({ name, animal, breed, media, location, id }) {
       </div>
       <div className="info">
         <h1>{name}</h1>
-        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
       </div>
     </Link>
-  )
-}
+  );
+};
 
+export default Pet;
