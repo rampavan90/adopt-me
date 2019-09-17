14c128bc9180c4a7de80d761df9ab838e6471a64
Ram Pavan FLIP <rampavan.kumar@learnwithflip.com>
2019-09-17 17:25:17 +0530

    finished Brian's intro to react course

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
-import Results from './Results';
+import Results from "./Results";
 import useDropdown from "./useDropdown";
-
-
+import ThemeContext from "./ThemeContext";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
@@ -11,8 +10,9 @@ const SearchParams = () => {
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
   const [pets, setPets] = useState([]);
+  const [theme, setTheme] = useContext(ThemeContext);
 
-  async function requestPets(){
+  async function requestPets() {
     const { animals } = await pet.animals({
       location,
       breed,
@@ -34,10 +34,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        requestPets();
-        }}>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -49,7 +51,20 @@ const SearchParams = () => {
         </label>
         <AnimalDropdown />
         <BreedDropdown />
-        <button>Submit</button>
+        <label htmlFor="theme">
+          Theme
+          <select
+            value={theme}
+            onChange={e => setTheme(e.target.value)}
+            onBlur={e => setTheme(e.target.value)}
+          >
+            <option value="peru">peru</option>
+            <option value="darkblue">darkblue</option>
+            <option value="mediumorchid">mediumorchid</option>
+            <option value="chartreuse">chartreuse</option>
+          </select>
+        </label>
+        <button style={{ backgroundColor: theme }}>Submit</button>
       </form>
       <Results pets={pets} />
     </div>
